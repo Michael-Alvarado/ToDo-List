d712cbf75749983948d5d71f35fcd48c92e65d6f
mkdemon5 <alvaradomike5@gmail.com>
2022-11-07 21:45:03 -0600

    updated server.js file

--- server.js
+++ server.js
@@ -1,4 +1,43 @@
-const bootstrap = require('bootstrap');
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+// const bootstrap = require('bootstrap');
+
+// Create a new sequelize store using the express-session package
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+	secret: 'We need to fix this later',
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize,
+	}),
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+sequelize.sync({ force: false }).then(() => {
+	app.listen(PORT, () => console.log('Now listening'));
+});
