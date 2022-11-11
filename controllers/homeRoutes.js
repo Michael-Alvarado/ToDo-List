ffed5f573d6b8d589574f35bf40a24ca5267fe77
Krumbelievable <krumbach.will@gmail.com>
2022-11-10 18:45:42 -0600

    revised routes in homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -40,43 +40,13 @@ router.get('/chore/:id', async (req, res) => {
 
 		const project = projectData.get({ plain: true });
 
-		res.render('project', {
-			...project,
+		res.render('chore', {
+			...chore,
 			logged_in: req.session.logged_in,
 		});
 	} catch (err) {
 		res.status(500).json(err);
 	}
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-	try {
-		// Find the logged in user based on the session ID
-		const userData = await User.findByPk(req.session.user_id, {
-			attributes: { exclude: ['password'] },
-			include: [{ model: Project }],
-		});
-
-		const user = userData.get({ plain: true });
-
-		res.render('profile', {
-			...user,
-			logged_in: true,
-		});
-	} catch (err) {
-		res.status(500).json(err);
-	}
-});
-
-router.get('/login', (req, res) => {
-	// If the user is already logged in, redirect the request to another route
-	if (req.session.logged_in) {
-		res.redirect('/profile');
-		return;
-	}
-
-	res.render('login');
 });
 
 module.exports = router;
