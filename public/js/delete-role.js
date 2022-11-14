b604837dce4166971d9d818df45f688304d5d320
Krumbelievable <krumbach.will@gmail.com>
2022-11-14 16:06:04 -0600

    fixed delete route

--- public/js/delete-role.js
+++ public/js/delete-role.js
@@ -1,29 +1,35 @@
-const deleteRoleHandler = async (event) =>{
-    event.preventDefault();
+const deleteRoleHandler = async (event) => {
+	event.preventDefault();
 
-    /* console.log (window.location.toString().split('/')) in google chrome and it returns an array splitted by '/' ['http://localhost:3001/edit/user/:id'] */
-    /* array[array.length -1] will return the correct /:id */
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
+	/* console.log (window.location.toString().split('/')) in google chrome and it returns an array splitted by '/' ['http://localhost:3001/edit/user/:id'] */
+	/* array[array.length -1] will return the correct /:id */
+	const id = window.location.toString().split('/')[
+		window.location.toString().split('/').length - 1
+	];
 
-    const response = await fetch(`api/family/${id}`, {
-        method: 'DELETE',
-        body: JSON.stringify({
-            family_id: id,
-        }),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+	console.log(
+		window.location.toString().split('/')[
+			window.location.toString().split('/').length - 1
+		]
+	);
 
-    if (response.ok){
-        document.location.replace('/user');
-    } else {
-        alert(response.statusText)
-    }
-}
+	const response = await fetch(`/api/family/${id}`, {
+		method: 'DELETE',
+		body: JSON.stringify({
+			family_id: id,
+		}),
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	});
+
+	if (response.ok) {
+		document.location.replace('/user');
+	} else {
+		alert(response.statusText);
+	}
+};
 
 document
-    .querySelector('.delete-role-btn')
-    .addEventListener('click', deleteRoleHandler)
+	.querySelector('.delete-role-btn')
+	.addEventListener('click', deleteRoleHandler);
